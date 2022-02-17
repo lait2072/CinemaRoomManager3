16ef040722104b46f1e8913122d1127a80c1b5e3
Cinema <lait2072@gmail.com>
2022-02-18 01:25:01 +0300

    Initial Commit

---
+++ Topics/Errors in programs/Fix compile-time errors/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String line1 = scanner.nextLine();
+        String line2 = scanner.nextLine();
+
+        System.out.println(line2);
+        System.out.println(line1);
+    }
+
+    public static void checkTheCode (String input) {
+        boolean result = Boolean.parseBoolean(input);
+        result = !result;
+        String output = String.valueOf(result);
+        System.out.println(output);
+    }
+    int size = numbers == null ? 0 : numbers.length;
+}
